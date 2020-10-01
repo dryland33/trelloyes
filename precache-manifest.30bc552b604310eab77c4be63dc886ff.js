b847ef95bfde8e2d02462381d59605d40badad0a
Rylan Wassem <dryland28@gmail.com>
2020-10-01 19:31:07 -0400

    Updates

---
+++ precache-manifest.30bc552b604310eab77c4be63dc886ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b14b898e13f8e7ffdfbfffaae1ab51",
+    "url": "/trelloyes/index.html"
+  },
+  {
+    "revision": "b3c45e114fd314fb8e9b",
+    "url": "/trelloyes/static/css/main.18d32b8b.chunk.css"
+  },
+  {
+    "revision": "084a879dcb245196649d",
+    "url": "/trelloyes/static/js/2.706faa7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trelloyes/static/js/2.706faa7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c45e114fd314fb8e9b",
+    "url": "/trelloyes/static/js/main.83288c5f.chunk.js"
+  },
+  {
+    "revision": "5d42d18be2c61f5ec424",
+    "url": "/trelloyes/static/js/runtime-main.0843b9cf.js"
+  }
+]);
